82cab297f64679003c2fdf7427a92a413539dee1
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-11 20:10:55 +0530

    [Rajashr]Add:Create Table-AddressBook

--- AddressBook/AddressBook/AddressBookData.cs
+++ AddressBook/AddressBook/AddressBookData.cs
@@ -9,6 +9,9 @@ namespace AddressBook
 {
     public class AddressBookData
     {
+        /// <summary>
+        /// Create Database-Addressbook Service
+        /// </summary>
         public static void Create_Database()
         {
             try
@@ -23,6 +26,25 @@ namespace AddressBook
             catch (Exception e)
             {
                 Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+        /// <summary>
+        /// Created Table in addressbook service database
+        /// </summary>
+        public static void CreateTables()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=AD-PC\SQLEXPRESS; Initial Catalog =AddressbookService; Integrated Security = True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Create table AddressBook(id int identity(1,1)primary key,First_Name varchar(200),Last_Name varchar(200),Address varchar(200), City varchar(200), State varchar(200), Zip varchar(200), Phone_Number varchar(50), Email varchar(200)); ", con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Employee Payroll table has been  created successfully!");
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating table:" + e.Message + "\t");
             }
         }
     }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -2,12 +2,15 @@
 using AddressBook;
 
 Console.WriteLine("*************AddressBook System******************");
-Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n");
+Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create Table");
 int op = Convert.ToInt16(Console.ReadLine());
 switch(op)
 {
     case 1:
         AddressBookData.Create_Database();
+        break;
+    case 2:
+        AddressBookData.CreateTables();
         break;
     default:
         Console.WriteLine("Wrong Choice");
